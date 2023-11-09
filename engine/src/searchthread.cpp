6dbaf4534d4cc65ab631b678e7365a5032f8911f
Jinyao Chen <38099452+cjy513203427@users.noreply.github.com>
2023-11-09 13:18:26 +0100

    delete comment

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -412,7 +412,6 @@ void SearchThread::thread_iteration()
         create_mini_batch(rootNode);
         // create_mini_batch(rootNodeLarge);
         #ifndef SEARCH_UCT
-        // cout<<"newNodes->size():"<<newNodes->size()<<endl;
         if (newNodes->size() != 0) {
             net->predict(inputPlanes, valueOutputs, probOutputs, auxiliaryOutputs);
             set_nn_results_to_child_nodes();
@@ -546,7 +545,6 @@ void SearchThread::select_unevaluated_leafState_priority(Node* rootNode){
 	// Priority means higher visit counts(based on small tree)
 	// For each node have potential nodes, choose important nodes which has the most qvalues. The best move has the most visits. Subsequent nodes and opponent move are also important. Future moves take into account.
     // Get final rootNode
-    // cout<< "--------------------------t->iterate_all_nodes_bfs(rootNodeBFS)--------------------------" << endl;
 	std::multimap<unsigned int, Node*, std::greater<unsigned int>> rootNodeMap = this->iterate_all_nodes_bfs(rootNode);
 	// Todo: Exclude nodes whose qVal are -1, after excluding, rootNodeMap's size is 0?
 	// get key through hashKey. Need for loop map.
